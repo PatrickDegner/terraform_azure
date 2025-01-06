f1df3971dcbc9c8921778012154f6909812a25c6
PatrickDegner <Acaer@gmx.net>
2025-01-06 19:06:40 +0100

    added subscription id

--- providers.tf
+++ providers.tf
@@ -10,6 +10,7 @@ terraform {
 
   # Configure the Remote Backend
   backend "azurerm" {
+    subscription_id      = "d866ec5d-0016-4b05-acd4-1a77aafcfef9"
     resource_group_name  = "terraform-state-rg-patrick"
     storage_account_name = "terraformstatepatrick"
     container_name       = "tfstate"
