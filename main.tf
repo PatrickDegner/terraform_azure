1b3630b01be81eb005ff925957d0afefefd6cd16
PatrickDegner <Acaer@gmx.net>
2025-01-06 20:25:11 +0100

    added apply

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-# resource "azurerm_storage_account" "tfstate_sa" {
-#   name                     = "testaccount01patrick"
-#   resource_group_name      = var.resource_group_name
-#   location                 = var.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-# }
+resource "azurerm_storage_account" "tfstate_sa" {
+  name                     = "testaccount01patrick"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
 
-# resource "azurerm_storage_container" "tfstate_container" {
-#   name                  = "testcontainer01patrick"
-#   storage_account_id    = azurerm_storage_account.tfstate_sa.id
-#   container_access_type = "private"
-# }
+resource "azurerm_storage_container" "tfstate_container" {
+  name                  = "testcontainer01patrick"
+  storage_account_id    = azurerm_storage_account.tfstate_sa.id
+  container_access_type = "private"
+}
